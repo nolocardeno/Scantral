28fec0c1ac9c20039d8e178ae786752848354d44
nolorubio23 <mcarsan165@g.educaand.es>
2026-03-03 08:30:58 +0100

    Creada la estructura Backend (Proyecto Springboot)

---
+++ backend/src/main/java/com/nolocardeno/backend/BackendDelProyectoFinalApplication.java
@@ -0,0 +1,13 @@
+package com.nolocardeno.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendDelProyectoFinalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendDelProyectoFinalApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/nolocardeno/backend/BackendDelProyectoFinalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nolocardeno.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendDelProyectoFinalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
