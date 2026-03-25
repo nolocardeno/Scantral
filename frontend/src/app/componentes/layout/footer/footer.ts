9a120ea069d93d16f55802c6c63aa9a681270f3b
nolorubio23 <mcarsan165@g.educaand.es>
2026-03-25 23:14:17 +0100

    Creación de lógica del componente de Footer

---
+++ frontend/src/app/componentes/layout/footer/footer.ts
@@ -0,0 +1,31 @@
+// --------------------------------------------------------------------------
+// IMPORTS
+// --------------------------------------------------------------------------
+import { Component, input } from '@angular/core';
+import { FaIconComponent } from '@fortawesome/angular-fontawesome';
+import {
+  faGithub,
+  faLinkedinIn,
+  faInstagram,
+  faXTwitter,
+} from '@fortawesome/free-brands-svg-icons';
+
+// --------------------------------------------------------------------------
+// COMPONENTE: FOOTER
+// --------------------------------------------------------------------------
+@Component({
+  selector: 'app-footer',
+  imports: [FaIconComponent],
+  templateUrl: './footer.html',
+  styleUrl: './footer.scss',
+})
+export class FooterComponent {
+  variante = input<'default' | 'con-sidebar'>('default');
+  readonly currentYear = new Date().getFullYear();
+
+  // --- Iconos Font Awesome (solo los necesarios) ---
+  readonly faGithub = faGithub;
+  readonly faLinkedinIn = faLinkedinIn;
+  readonly faInstagram = faInstagram;
+  readonly faXTwitter = faXTwitter;
+}
