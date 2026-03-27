139dec87b9c9d1052a622c9bb01eea67fa4ed9f8
nolorubio23 <mcarsan165@g.educaand.es>
2026-03-27 10:50:58 +0100

    Variante de header con card de usuario añadida para sesión iniciada

--- frontend/src/app/componentes/layout/header/header.ts
+++ frontend/src/app/componentes/layout/header/header.ts
@@ -1,17 +1,23 @@
 // --------------------------------------------------------------------------
 // IMPORTS
 // --------------------------------------------------------------------------
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { RouterLink } from '@angular/router';
 import { ButtonComponent } from '../../shared/button/button';
+import { CardUsuarioComponent } from '../../shared/card-usuario/card-usuario';
+import { AuthModalService } from '../../../servicios/auth-modal.service';
+import { AuthService } from '../../../servicios/auth.service';
 
 // --------------------------------------------------------------------------
 // COMPONENTE: HEADER
 // --------------------------------------------------------------------------
 @Component({
   selector: 'app-header',
-  imports: [RouterLink, ButtonComponent],
+  imports: [RouterLink, ButtonComponent, CardUsuarioComponent],
   templateUrl: './header.html',
   styleUrl: './header.scss',
 })
-export class HeaderComponent {}
+export class HeaderComponent {
+  protected readonly authModal = inject(AuthModalService);
+  protected readonly authService = inject(AuthService);
+}
