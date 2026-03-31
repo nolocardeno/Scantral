f6f3fddf99746e62ce7cf8dc156996bab09f6b59
nolorubio23 <mcarsan165@g.educaand.es>
2026-04-01 01:09:06 +0200

    Creadas variantes de botón para componente Page-header

---
+++ frontend/src/app/components/shared/button/button.ts
@@ -0,0 +1,19 @@
+// --------------------------------------------------------------------------
+// IMPORTS
+// --------------------------------------------------------------------------
+import { Component, input } from '@angular/core';
+
+// --------------------------------------------------------------------------
+// COMPONENTE: BUTTON (Reutilizable)
+// --------------------------------------------------------------------------
+@Component({
+  selector: 'app-button',
+  templateUrl: './button.html',
+  styleUrl: './button.scss',
+})
+export class ButtonComponent {
+  variant = input<'primary' | 'secondary' | 'ghost' | 'text' | 'icon'>('primary');
+  size = input<'sm' | 'md' | 'lg' | 'auth' | 'cta'>('md');
+  type = input<'button' | 'submit'>('button');
+  disabled = input<boolean>(false);
+}
